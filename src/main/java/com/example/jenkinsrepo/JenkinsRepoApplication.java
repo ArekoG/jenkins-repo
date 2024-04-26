89fc34a90a513ef3ac5e4a9a43189d1a7e3ce3d1
arekog <arekgajda1996@gmail.com>
2024-04-26 15:34:07 +0200

    initial

---
+++ src/main/java/com/example/jenkinsrepo/JenkinsRepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkinsrepo/JenkinsRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
